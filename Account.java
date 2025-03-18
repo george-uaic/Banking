b74bdc92554de4242bc8cfec72ac601c09c3b4de
george-uaic <george.uaic@gmail.com>
2025-03-18 09:04:47 +0200

    account

---
+++ Account.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Account
+{
+    private ArrayList<String> transactions;
+    private String accountID;
+    private float balance;
+    
+    Account()
+    {
+        accountID = UUID.randomUUID().toString();
+        balance = 0.0f;
+    }
+
+    public String getAccountID()
+    {
+        return accountID;
+    }
+
+    public float getBalance()
+    {
+        return balance;
+    }
+
+    public void addTransaction(Transaction transaction)
+    {
+        transactions.add(transaction.getTransactionID());
+    }
+
+    public ArrayList<String> getTransactions()
+    {
+        return transactions;
+    }
+}
